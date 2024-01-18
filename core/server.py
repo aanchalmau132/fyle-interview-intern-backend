d21071e37072ec29b5f8215d94be2111e59f1cf5
aanchalmau132 <121788593+aanchalmau132@users.noreply.github.com>
2024-01-18 18:35:38 +0530

    server

--- core/server.py
+++ core/server.py
@@ -10,6 +10,8 @@ from sqlalchemy.exc import IntegrityError
 
 app.register_blueprint(student_assignments_resources, url_prefix='/student')
 app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
+app.register_blueprint(principal_assignments_resources, url_prefix='/principal')
+
 
 
 @app.route('/')
